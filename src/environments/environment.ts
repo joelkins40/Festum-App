74d95ffbdd8cd75e9e73b63fb6859df69d05d375
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-10-30 14:53:22 -0600

    feat: agregar clave de API de Geoapify al entorno de configuración

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: false,
   apiUrl: '/api', // Usando proxy para desarrollo
   appName: 'Festum App',
-  version: '1.0.0'
+  version: '1.0.0',
+  geoapifyApiKey: process.env['GEOAPIFY_API_KEY'] || ''
 };
